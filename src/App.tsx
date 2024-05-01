654fab8f6214605a3804546e73c584344bf21e5a
Victor Marques <victor.marques@stoom.com.br>
2024-05-01 12:11:17 -0300

    refactor: resetando o projeto para inicio de implementacao da aplicacao em si

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Provider as ReduxProvider } from "react-redux";
 
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
+import { Player } from "./components/Player";
 
 function App() {
 
   return (
     <ReduxProvider store={store}>
-      <TodoList/>
-      <AddTodo/>
+      <Player/>
     </ReduxProvider>
   )
 }
